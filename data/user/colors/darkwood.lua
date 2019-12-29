42d8d568a32f59acfb2d6dbb26578d181f8fc6e4
monolifed <6624464+monolifed@users.noreply.github.com>
2019-12-29 13:19:31 +0300

    Create darkwood.lua

---
+++ data/user/colors/darkwood.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background     = { common.color "#211b13" }
+style.background2    = { common.color "#1f1911" }
+style.background3    = { common.color "#1f1911" }
+style.text           = { common.color "#cdb29d" }
+style.caret          = { common.color "#698fce" }
+style.accent         = { common.color "#698fce" }
+style.dim            = { common.color "#5c4e38" }
+style.divider        = { common.color "#242223" }
+style.selection      = { common.color "#5c4e38" }
+style.line_number    = { common.color "#5c4e38" }
+style.line_number2   = { common.color "#af9573" }
+style.line_highlight = { common.color "#2e2513" }
+style.scrollbar      = { common.color "#5c4e38" }
+style.scrollbar2     = { common.color "#698fce" }
+
+style.syntax["normal"]   = { common.color "#cdb29d" }
+style.syntax["symbol"]   = { common.color "#cdb29d" }
+style.syntax["comment"]  = { common.color "#997f58" }
+style.syntax["keyword"]  = { common.color "#a96ede" }
+style.syntax["keyword2"] = { common.color "#a93c3c" }
+style.syntax["number"]   = { common.color "#ba7125" }
+style.syntax["literal"]  = { common.color "#ba8225" }
+style.syntax["string"]   = { common.color "#b0b33e" }
+style.syntax["operator"] = { common.color "#cdb29d" }
+style.syntax["function"] = { common.color "#698fce" }
+
