764b43494ed47b783638818288a99b85b1dc0d07
rxi <rxi@users.noreply.github.com>
2020-03-07 15:53:54 +0000

    Moved .c, .lua and .md language syntaxes from core.syntax to plugins

---
+++ data/plugins/language_md.lua
@@ -0,0 +1,20 @@
+local syntax = require "core.syntax"
+
+syntax.add {
+  files = { "%.md$", "%.markdown$" },
+  patterns = {
+    { pattern = "\\.",                    type = "normal"   },
+    { pattern = { "<!%-%-", "%-%->" },    type = "comment"  },
+    { pattern = { "```", "```" },         type = "string"   },
+    { pattern = { "`", "`", "\\" },       type = "string"   },
+    { pattern = { "~~", "~~", "\\" },     type = "keyword2" },
+    { pattern = "%-%-%-+",                type = "comment" },
+    { pattern = "%*%s+",                  type = "operator" },
+    { pattern = { "%*", "[%*\n]", "\\" }, type = "operator" },
+    { pattern = { "%_", "[%_\n]", "\\" }, type = "keyword2" },
+    { pattern = "#.-\n",                  type = "keyword"  },
+    { pattern = "!?%[.*%]%(.*%)",         type = "function" },
+    { pattern = "https?://%S+",           type = "function" },
+  },
+  symbols = { },
+}
