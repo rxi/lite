762c1e2b697a3e71bec1643fc2b21b11bbbb9548
rxi <rxi@users.noreply.github.com>
2020-05-07 21:46:21 +0100

    Fixed RootView's EmptyView keymap text
    Broke due to command renaming

--- data/core/rootview.lua
+++ data/core/rootview.lua
@@ -15,8 +15,8 @@ function EmptyView:draw()
   local x, y, w, h = pos.x, pos.y, self.size.x, self.size.y
   local _, y = common.draw_text(style.big_font, style.dim, "empty", "center", x, y, w, h)
   local lines = {
-    { fmt = "%s to run a command", cmd = "core:do-command" },
-    { fmt = "%s to open a file from the project", cmd = "core:open-project-file" },
+    { fmt = "%s to run a command", cmd = "core:command-finder" },
+    { fmt = "%s to open a file from the project", cmd = "core:file-finder" },
   }
   local th = style.font:get_height()
   for _, line in ipairs(lines) do
