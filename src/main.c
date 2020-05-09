143f8867a13a35f5688ad7c97717dddf8bc58cd2
rxi <rxi@users.noreply.github.com>
2020-05-09 16:29:34 +0100

    Version 1.03

--- src/main.c
+++ src/main.c
@@ -107,7 +107,7 @@ int main(int argc, char **argv) {
   }
   lua_setglobal(L, "ARGS");
 
-  lua_pushstring(L, "1.02");
+  lua_pushstring(L, "1.03");
   lua_setglobal(L, "VERSION");
 
   lua_pushstring(L, SDL_GetPlatform());
