92b3b5ba86d7a3b47ce1d1b3bf45b4cbb9c15639
demotulatingswan <demotulatingswan@users.noreply.github.com>
2020-05-15 20:18:07 +0200

    Fixed various issues in exec:replace

--- data/plugins/exec.lua
+++ data/plugins/exec.lua
@@ -2,11 +2,21 @@ local core = require "core"
 local command = require "core.command"
 
 
-local function exec(cmd)
+local function exec(cmd, keep_newline)
   local fp = io.popen(cmd, "r")
   local res = fp:read("*a")
   fp:close()
-  return res:gsub("%\n$", "")
+  return keep_newline and res or res:gsub("%\n$", "")
+end
+
+
+local function printfb_quote(str)
+  local sub = {
+    ["\\"] = "\\\\",
+    [string.char(0)] = "\\0000",
+    ["'"] = "'\\''",
+  }
+  return "'" .. str:gsub(".", sub) .. "'"
 end
 
 
@@ -20,7 +30,10 @@ command.add("core.docview", {
   ["exec:replace"] = function()
     core.command_view:enter("Replace With Result Of Command", function(cmd)
       core.active_view.doc:replace(function(str)
-        return exec(string.format("echo %q | %s", str, cmd))
+        return exec(
+          "printf %b " .. printfb_quote(str:gsub("%\n$", "") .. "\n") .. " | " .. cmd,
+          str:find("%\n$")
+        )
       end)
     end)
   end,
