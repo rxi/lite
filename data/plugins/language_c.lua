611d23af3da0fa0ca816a3d51621c52f504abb44
dimooski <dimooski@gmail.com>
2020-05-31 19:25:54 +0200

    Add more C++ file extensions for syntax highlighting

--- data/plugins/language_c.lua
+++ data/plugins/language_c.lua
@@ -1,7 +1,7 @@
 local syntax = require "core.syntax"
 
 syntax.add {
-  files = { "%.c$", "%.h$", "%.inl$", "%.cpp$", "%.hpp$" },
+  files = { "%.c$", "%.cc$", "%.cpp$", "%.cxx$", "%.c++$", "%.h$", "%.hh$", "%.hpp$", "%.inl$" }
   comment = "//",
   patterns = {
     { pattern = "//.-\n",               type = "comment"  },
