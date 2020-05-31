26c2f8421d018e7b4ab22e67561301a883c2af72
dimooski <dimooski@gmail.com>
2020-05-31 19:27:30 +0200

    .

--- data/plugins/language_c.lua
+++ data/plugins/language_c.lua
@@ -1,7 +1,7 @@
 local syntax = require "core.syntax"
 
 syntax.add {
-  files = { "%.c$", "%.cc$", "%.cpp$", "%.cxx$", "%.c++$", "%.h$", "%.hh$", "%.hpp$", "%.inl$" }
+  files = { "%.c$", "%.cc$", "%.cpp$", "%.cxx$", "%.c++$", "%.h$", "%.hh$", "%.hpp$", "%.inl$" },
   comment = "//",
   patterns = {
     { pattern = "//.-\n",               type = "comment"  },
