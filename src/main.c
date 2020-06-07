db471c05547f060acbaa3001d235588431da5bef
rxi <rxi@users.noreply.github.com>
2020-06-07 14:02:45 +0100

    Version 1.07

--- src/main.c
+++ src/main.c
@@ -100,7 +100,7 @@ int main(int argc, char **argv) {
   }
   lua_setglobal(L, "ARGS");
 
-  lua_pushstring(L, "1.06");
+  lua_pushstring(L, "1.07");
   lua_setglobal(L, "VERSION");
 
   lua_pushstring(L, SDL_GetPlatform());
