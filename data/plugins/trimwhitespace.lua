ae48049695e2a74721323f608e439fb5f5b89da4
rxi <rxi@users.noreply.github.com>
2020-06-19 12:09:00 +0100

    Changed `trimwhitespace` to never cause caret to reposition

--- data/plugins/trimwhitespace.lua
+++ data/plugins/trimwhitespace.lua
@@ -4,9 +4,16 @@ local Doc = require "core.doc"
 
 
 local function trim_trailing_whitespace(doc)
+  local cline, ccol = doc:get_selection()
   for i = 1, #doc.lines do
     local old_text = doc:get_text(i, 1, i, math.huge)
     local new_text = old_text:gsub("%s*$", "")
+
+    -- don't remove whitespace which would cause the caret to reposition
+    if cline == i and ccol > #new_text then
+      new_text = old_text:sub(1, ccol - 1)
+    end
+
     if old_text ~= new_text then
       doc:insert(i, 1, new_text)
       doc:remove(i, #new_text + 1, i, math.huge)
